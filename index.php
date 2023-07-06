662d30be0d593d5929ba4f6603c35db6d632eedd
Nick Spanos <nickspanosf1@gmail.com>
2023-07-06 11:06:25 +0300

    add js files

--- index.php
+++ index.php
@@ -1167,26 +1167,26 @@
 <!-- footer-end -->
 
 <!-- JS here -->
-<script src="js/vendor/modernizr-3.5.0.min.js"></script>
-<script src="js/vendor/jquery-3.6.0.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/ajax-form.js"></script>
-<script src="js/paroller.js"></script>
-<script src="js/wow.min.js"></script>
-<script src="js/js_isotope.pkgd.min.js"></script>
-<script src="js/imagesloaded.min.js"></script>
-<script src="js/parallax.min.js"></script>
-<script src="js/jquery.waypoints.min.js"></script>
-<script src="js/jquery.counterup.min.js"></script>
-<script src="js/jquery.scrollUp.min.js"></script>
-<script src="js/jquery.meanmenu.min.js"></script>
-<script src="js/parallax-scroll.js"></script>
-<script src="js/swiper.min.js"></script>
-<script src="js/aos.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/element-in-view.js"></script>
-<script src="js/main.js"></script>
+<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/slick.min.js"></script>
+<script src="assets/js/ajax-form.js"></script>
+<script src="assets/js/paroller.js"></script>
+<script src="assets/js/wow.min.js"></script>
+<script src="assets/js/js_isotope.pkgd.min.js"></script>
+<script src="assets/js/imagesloaded.min.js"></script>
+<script src="assets/js/parallax.min.js"></script>
+<script src="assets/js/jquery.waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.min.js"></script>
+<script src="assets/js/jquery.scrollUp.min.js"></script>
+<script src="assets/js/jquery.meanmenu.min.js"></script>
+<script src="assets/js/parallax-scroll.js"></script>
+<script src="assets/js/swiper.min.js"></script>
+<script src="assets/js/aos.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/element-in-view.js"></script>
+<script src="assets/js/main.js"></script>
 </body>
 </html>
