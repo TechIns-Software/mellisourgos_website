50ef0970e4c40f76400c905df29055549d2eb6f0
Nick Spanos <nickspanosf1@gmail.com>
2023-08-21 18:27:44 +0300

    create about page

---
+++ about/index.php
@@ -0,0 +1,147 @@
+<?php
+include_once  '../commonParts.php';
+$str = '../';
+$title = 'Σχετικά με εμένα';
+navbar($str,$title);
+?>
+
+
+    <!-- rts about us section start -->
+    <div class="rts-about-area rts-section-gap  ">
+        <div class="container">
+            <div class="row  align-items-center">
+                <div class="col-lg-12">
+                    <div class="about-progress-inner">
+                        <div class="title-area">
+                            <span>Σχετικά με εμένα</span>
+                            <h2 class="title">Λίγα λόγια για εμένα </h2>
+                        </div>
+                        <!-- inner start -->
+                        <div class="inner">
+                            <p class="disc">Ονομάζομαι <b> Μελισσουργός Κωνσταντίνος </b>, είμαι γεννημένος στην Αθήνα
+                                και από νωρίς είχα πάθος για τη γνώση, τη δημιουργία και την καινοτομία.
+                                Εκπαίδευση που ενώνει τα πεδία της Χρηματοοικονομικής και του Προγραμματισμού.</p>
+
+
+
+                            <p class="disc">Επενδύω συνεχώς στην ανάπτυξη του εαυτού του,έχοντας παραστεί σε πλήθος
+                                εκπαιδευτικών σεμιναρίων για το Management σε Ελλάδα και Αμερική.
+                                Η εργασιακή μου πορεία μιλά από μόνη της για τον επαγγελματισμό και τη
+                                δέσμευσή μου. Έχοντας συνεργαστεί με κορυφαία ονόματα στο χώρο, όπως η Alpha Bank και η
+                                NN Hellas, έχω αποκτήσει πολύτιμη εμπειρία και γνώσεις που με καθιστούν αξιόπιστο και  ειδικό
+                                στον ασφαλιστικό τομέα. </p>
+
+                        </div>
+                        <!-- end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts about us section end -->
+
+    <div class="rts-inner-blog ptb--70">
+        <div class="container custom">
+            <div class="row">
+                <div class="col-lg-12 pr-35 md-pr-15 md-mt-50">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="blog-details">
+                                <div class="blog-full">
+                                    <blockquote>
+                                        <p class="margin-0">Το να είμαστε η καλύτερη ομάδα, έχει σημασία<br>
+                                            <cite>NN Hellas</cite>
+                                        </p>
+                                    </blockquote>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts faq section area -->
+    <div class="rts-faq-section rts-section-gap rts-faq-bg bg_image">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-12">
+                    <div class="faq-two-inner">
+                        <div class="title-area-faq">
+                            <span class="sub">Συχνές Ερωτήσεις</span>
+                            <h2 class="title">We Are Experienced
+                                <span class="sm-title">Business <span>Solution</span></span>
+                            </h2>
+                        </div>
+                        <!-- faq accordion area -->
+                        <div class="faq-accordion-area">
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                                                data-bs-target="#collapseOne" aria-expanded="true"
+                                                aria-controls="collapseOne">
+                                            <span>01. </span> What should i included my personal details?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show"
+                                         aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            Neque partrient nascetur facilisis suscipit ridiculus agna lobortis
+                                            imperdiet vivamus est aliquam euismod nector quam convallis ornare justo
+                                            service lifereu visionary sources unleash online businesss solutions
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingTwo">
+                                        <button class="accordion-button collapsed" type="button"
+                                                data-bs-toggle="collapse" data-bs-target="#collapseTwo"
+                                                aria-expanded="false" aria-controls="collapseTwo">
+                                            <span>02. </span> Where i can find my business growth result?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseTwo" class="accordion-collapse collapse"
+                                         aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            Neque partrient nascetur facilisis suscipit ridiculus agna lobortis
+                                            imperdiet vivamus est aliquam euismod nector quam convallis ornare justo
+                                            service lifereu visionary sources unleash online businesss solutions
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingThree">
+                                        <button class="accordion-button collapsed" type="button"
+                                                data-bs-toggle="collapse" data-bs-target="#collapseThree"
+                                                aria-expanded="false" aria-controls="collapseThree">
+                                            <span>03. </span> Did you get any business consultant?
+                                        </button>
+                                    </h2>
+                                    <div id="collapseThree" class="accordion-collapse collapse"
+                                         aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            Neque partrient nascetur facilisis suscipit ridiculus agna lobortis
+                                            imperdiet vivamus est aliquam euismod nector quam convallis ornare justo
+                                            service lifereu visionary sources unleash online businesss solutions
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- faq accordion area end -->
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="thumbnail-faq-four">
+                        <img src="assets/images/faq/02.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts faq section area End -->
+
+<?php
+footer($str);
+?>
---
+++ commonParts.php
@@ -0,0 +1,322 @@
+<?php
+
+function navbar($str='../',$title){
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>  <?= $title ?>  | Konstantinos Melissourgos </title>
+        <link rel="shortcut icon" type="image/x-icon" href="<?= $str ?>assets/images/fav.png">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/plugins/swiper.min.css">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/plugins/fontawesome-5.css">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/plugins/animate.min.css">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/plugins/unicons.css">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/vendor/bootstrap.min.css">
+        <link rel="stylesheet" href="<?= $str ?>assets/css/style.css">
+    </head>
+
+    <body class="index-seven hr">
+    <!-- start header area -->
+    <!-- start header area -->
+    <header class="header--sticky header-one six">
+        <div class="header-top header-top-one bg-1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 d-xl-block d-none">
+                        <div class="left">
+                            <p class="left-text"> </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 d-xl-block d-none">
+                        <div class="right">
+                            <div class="mail">
+                                <i class="fas fa-envelope"></i>
+                                <a href="#">Melissourgoskonstantinos@gmail.com</a>
+                            </div>
+                            <div class="call">
+                                <i class="fas fa-phone-alt"></i>
+                                <a href="#">
+                                    Τηλ : +306944314855.
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-main-one">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-4 col-md-4 col-sm-4 col-4">
+                        <div class="thumbnail">
+                            <a href="<?= $str ?>index.php">
+                                <img src="<?= $str ?>assets/images/logo/logo-1.svg" alt="finbiz-logo">
+                            </a>
+                        </div>
+                    </div>
+                    <div class=" col-xl-9 col-lg-8 col-md-8 col-sm-8 col-8">
+                        <div class="main-header">
+                            <nav class="nav-main mainmenu-nav d-none d-xl-block">
+                                <ul class="mainmenu">
+
+                                    <li><a class="nav-item" href="<?= $str ?>index.php">Αρχική</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>vision/index.php">Οραμα</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>about/index.php">Σχετικά με εμένα</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>office/index.php">Γραφεία</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>career/index.php">Καριέρα</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>videos/index.php">Βίντεο</a></li>
+                                    <li><a class="nav-item" href="<?= $str ?>contact/index.php">Επικοινωνία</a></li>
+                                </ul>
+                            </nav>
+                            <div class="button-area">
+                                <div class="search-input-area ">
+                                    <div class="container">
+                                        <div class="search-input-inner">
+                                            <div class="input-div">
+                                                <input id="searchInput1" class="search-input" type="text" placeholder="Search by keyword or #">
+                                                <button><i class="far fa-search"></i></button>
+                                            </div>
+                                            <div id="close" class="search-close-icon"><i class="far fa-times"></i></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button id="menu-btn" class="menu rts-btn btn-primary-alta ml--20 ml_sm--5">
+                                    <img class="menu-dark" src="<?= $str ?>assets/images/icon/menu.png" alt="Menu-icon">
+                                    <img class="menu-light" src="<?= $str ?>assets/images/icon/menu-light.png" alt="Menu-icon">
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- End header area -->
+
+    <div id="side-bar" class="side-bar">
+        <button class="close-icon-menu"><i class="far fa-times"></i></button>
+        <!-- inner menu area desktop start -->
+        <div class="rts-sidebar-menu-desktop">
+            <a class="logo-1" href="<?= $str ?>index.php"><img class="logo" src="assets/images/logo/logo.svg" alt="melissourgos_logo"></a>
+
+            <div class="body d-none d-xl-block">
+                <p class="disc">
+                    Μάθε ν’ ακούς. Η ευκαιρία καμιά φορά χτυπάει την πόρτα σου πολύ σιγανά.
+                </p>
+                <div class="get-in-touch">
+                    <!-- title -->
+                    <div class="h6 title">Ελα σε επικοινωνία μαζί μας</div>
+                    <!-- title End -->
+                    <div class="wrapper">
+                        <!-- single -->
+                        <div class="single">
+                            <i class="fas fa-phone-alt"></i>
+                            <a href="#">+306944314855.</a>
+                        </div>
+                        <!-- single ENd -->
+                        <!-- single -->
+                        <div class="single">
+                            <i class="fas fa-envelope"></i>
+                            <a href="#" style="font-size: .9em">Melissourgoskonstantinos@gmail.com</a>
+                        </div>
+                        <!-- single ENd -->
+
+                        <!-- single -->
+                        <div class="single">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <a href="#">Λεωφ. Κηφισίας 131</a>
+                        </div>
+                        <!-- single ENd -->
+                    </div>
+                    <div class="social-wrapper-two menu">
+                        <a href="https://www.facebook.com/Melissourgos.Konstantinos"><i class="fab fa-facebook-f"></i></a>
+                        <!--                        TODO : ΠΡΕΠΕΙ ΝΑ ΒΑΛΟΥΜΕ ΤΟ ΤΙΚ ΤΟΚ-->
+                        <a href="https://www.instagram.com/melissourgos.k/"><i class="fab fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/in/konstantinos-melissourgos/"><i class="fab fa-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="body-mobile d-block d-xl-none">
+                <nav class="nav-main mainmenu-nav">
+                    <ul class="mainmenu">
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>index.php">Αρχική</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>vision/index.php">Όραμα</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>about/index.php">Σχετικά με εμάς</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>office/index.php">Γραφεία</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>career/index.php">Καριέρα</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>videos/index.php">Βίντεο</a></li>
+                        <li class="menu-item"><a class="menu-link" href="<?= $str ?>contact/index.php">Επικοινωνία</a></li>
+                    </ul>
+                </nav>
+                <div class="social-wrapper-two menu mobile-menu">
+                    <a href="https://www.facebook.com/Melissourgos.Konstantinos"><i class="fab fa-facebook-f"></i></a>
+                    <!--                        TODO : ΠΡΕΠΕΙ ΝΑ ΒΑΛΟΥΜΕ ΤΟ ΤΙΚ ΤΟΚ-->
+                    <a href="https://www.instagram.com/melissourgos.k/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/konstantinos-melissourgos/"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+        <!-- inner menu area desktop End -->
+    </div>
+
+    <div id="anywhere-home"></div>
+    <!-- ENd Header Area -->
+<?php
+}
+
+function footer($str){
+    ?>
+    <!-- start footer area -->
+    <!-- rts footer area start -->
+    <div class="rts-footer-area footer-one  bg-footer-one">
+        <div class="container bg-shape-f1">
+            <!-- footer call to action area -->
+            <!-- footer call to action area End -->
+            <!-- rts footer area -->
+            <div class="row pt--120 pt_sm--80 pb--80 pb_sm--40">
+                <div class="col-xl-4 col-md-6 col-sm-12 col-12">
+                    <div class="footer-one-single-wized seven">
+                        <div class="wized-title">
+                            <h5 class="title">Μενού</h5>
+                            <img src="assets/images/footer/under-title.png" alt="finbiz_footer">
+                        </div>
+                        <div class="quick-link-inner">
+                            <ul class="links">
+                                <li><a href="<?= $str ?>index.php"><i class="far fa-arrow-right"></i> Αρχική</a></li>
+                                <li><a href="<?= $str ?>vision/index.php"><i class="far fa-arrow-right"></i> Όραμα</a></li>
+                                <li><a href="<?= $str ?>about/index.php"><i class="far fa-arrow-right"></i> Σχετικά με εμένα</a></li>
+                                <li><a href="<?= $str ?>office/index.php"><i class="far fa-arrow-right"></i> Γραφεία</a></li>
+
+                            </ul>
+                            <ul class="links margin-left-70">
+                                <li><a href="<?= $str ?>career/index.php"><i class="far fa-arrow-right"></i>  Καριέρα</a></li>
+                                <li><a href="<?= $str ?>videos/index.php"><i class="far fa-arrow-right"></i> Βίντεο</a></li>
+                                <li><a href="<?= $str ?>contact/index.php"><i class="far fa-arrow-right"></i> Επικοινωνία</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer mid area -->
+                <div class="col-xl-4 col-md-6 col-sm-12 col-12">
+                    <div class="footer-one-single-wized mid-bg">
+                        <div class="wized-title">
+                            <h5 class="title">Ωρες Λειτουργίας</h5>
+                            <img src="<?= $str ?>assets/images/footer/under-title.png" alt="finbiz_footer">
+                        </div>
+                        <div class="opening-time-inner">
+                            <div class="single-opening">
+                                <p class="day">Δευτ. - Παρ. </p>
+                                <p class="time">09:00 - 17:00</p>
+                            </div>
+                            <div class="single-opening">
+                                <p class="day">Σάββατο</p>
+                                <p class="time">10:00 - 14:00</p>
+                            </div>
+
+                            <a href="#" class="rts-btn btn-primary contact-us">Επικοινωνία</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- footer mid area end -->
+
+                <!-- footer end area post -->
+                <div class="col-xl-4 col-md-6 col-sm-12 col-12">
+                    <div class="footer-one-single-wized seven margin-left-65">
+                        <div class="wized-title">
+                            <h5 class="title">Βρείτε μας εδώ</h5>
+                            <img src="<?= $str ?>assets/images/footer/under-title.png" alt="finbiz_footer">
+                        </div>
+                        <div class="get-in-touch">
+
+                            <div class="wrapper">
+                                <!-- single -->
+                                <div class="single">
+                                    <span>  Τηλ Επικοινωνίας:</span>
+                                    <a href="#">+306944314855.</a>
+                                </div>
+                                <!-- single ENd -->
+                                <!-- single -->
+                                <div class="single">
+                                    <span>  Email:</span>
+                                    <a href="#" style="font-size: .9em">Melissourgoskonstantinos@gmail.com</a>
+                                </div>
+                                <!-- single ENd -->
+
+                                <!-- single -->
+                                <div class="single">
+                                    <span>  Διεύθυνση:</span>
+                                    <a target="_blank" href="https://www.google.com/maps?q=%CE%9B%CE%B5%CF%89%CF%86.+%CE%9A%CE%B7%CF%86%CE%B9%CF%83%CE%AF%CE%B1%CF%82+131,+15124,%CE%9C%CE%B1%CF%81%CE%BF%CF%8D%CF%83%CE%B9">Λεωφ. Κηφισίας 131</a>
+                                </div>
+                                <!-- single ENd -->
+                            </div>
+                            <div class="social-wrapper-two menu">
+                                <a href="https://www.facebook.com/Melissourgos.Konstantinos"><i class="fab fa-facebook-f"></i></a>
+                                <!--                        TODO : ΠΡΕΠΕΙ ΝΑ ΒΑΛΟΥΜΕ ΤΟ ΤΙΚ ΤΟΚ-->
+                                <a href="https://www.instagram.com/melissourgos.k/"><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.linkedin.com/in/konstantinos-melissourgos/"><i class="fab fa-linkedin"></i></a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- footer end area post end-->
+            </div>
+            <!-- rts footer area End -->
+        </div>
+        <!-- copyright area start -->
+        <div class="rts-copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center">
+                            <p> <a target="_blank" href="https://techins.gr/"> TechIns </a> - Copyright <script>document.write(new Date().getFullYear()); </script>. All rights reserved.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- copyright area end -->
+    </div>
+    <!-- rts footer area end -->
+    <!-- ENd footer Area -->
+
+    <!-- start loader -->
+    <div class="loader-wrapper">
+        <div class="loader">
+        </div>
+        <div class="loader-section section-left"></div>
+        <div class="loader-section section-right"></div>
+    </div>
+    <!-- End loader -->
+
+    <!-- progress Back to top -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!-- progress Back to top End -->
+    <!-- scripts start form hear -->
+    <script src="<?= $str ?>assets/js/vendor/jquery.min.js"></script>
+    <script src="<?= $str ?>assets/js/vendor/jqueryui.js"></script>
+    <script src="<?= $str ?>assets/js/vendor/waypoint.js"></script>
+    <script src="<?= $str ?>assets/js/plugins/swiper.js"></script>
+    <script src="<?= $str ?>assets/js/plugins/counterup.js"></script>
+    <script src="<?= $str ?>assets/js/plugins/sal.min.js"></script>
+    <script src="<?= $str ?>assets/js/vendor/bootstrap.min.js"></script>
+    <script src="<?= $str ?>assets/js/plugins/contact.form.js"></script>
+    <!-- main Js -->
+    <script src="<?= $str ?>assets/js/main.js"></script>
+    <!-- scripts end form hear -->
+    </body>
+
+    </html>
+
+    <?php
+}
+
