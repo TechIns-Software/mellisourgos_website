78d81507e4c0a5ec2256e6648a840c8dbd2b9fcd
Nick Spanos <nickspanosf1@gmail.com>
2023-08-22 18:07:55 +0300

    office page

---
+++ office/index.php
@@ -0,0 +1,432 @@
+<?php
+include_once  '../commonParts.php';
+$str = '../';
+$title = 'Γραφεία';
+navbar($str,$title);
+?>
+
+
+    <!-- rts about us section start -->
+    <div class="rts-about-area rts-section-gap  ">
+        <div class="container">
+            <div class="row  align-items-center">
+                <div class="col-lg-12 col-md-12">
+                    <div class="about-progress-inner">
+                        <div class="title-area">
+                            <span> Γραφεία  </span>
+                            <h2 class="title"> Συνεργάτες</h2>
+                        </div>
+                        <!-- inner start -->
+                        <div class="inner">
+                            <p class="disc">Η επιτυχία μας δεν θα ήταν δυνατή χωρίς την αξιοσημείωτη συνεισφορά των συνεργατών μας.
+                              <br>  Εδώ στην NN Hellas είμαστε υπερήφανοι για την συνεργασία μας με αξιόλογους εταίρους που συμβάλλουν στην παροχή των καλύτερων προϊόντων και υπηρεσιών στους πελάτες μας.</p>
+
+                        </div>
+                        <!-- end -->
+                    </div>
+                </div>
+
+            </div>
+            <div class="row align-items-center py-3">
+                <div class="col-12 text-center">
+                    <h4>Τα γραφεία μας</h4>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Γαλάτσι
+                        </div>
+                        <div class="agent">
+                            Εμμανουήλ Σταυράκης
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Ελ. Βενιζέλου 16, 11147 ,Γαλάτσι" target="_blank">
+                                Ελ. Βενιζέλου 16, 11147
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:211-2123900 ">
+                                211-2123900
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            211-2123900
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn161@nnadvisors.gr ">
+                                nn161@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Γλυφάδα
+                        </div>
+                        <div class="agent">
+                            Ιωάννης Γιαννακόπουλος
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Βουλιαγμένης 41 και Λόντου, 16675,Γλυφάδα" target="_blank">
+                                Λεωφ. Βουλιαγμένης 41 και Λόντου, 16675
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-9646454 ">
+                                210-9646454
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-9646454
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn135@nnadvisors.gr">
+                                nn135@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Γλυφάδα
+                        </div>
+                        <div class="agent">
+                            Κωνσταντίνος Παπαμιχαήλ
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Ποσειδώνος και Α. Παπανδρέου 4, 16675,Γλυφάδα" target="_blank">
+                                Ποσειδώνος και Α. Παπανδρέου 4, 16675
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-8981870/3 ">
+                                210-8981870/3
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-8981870/3
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn162@nnadvisors.gr ">
+                                nn162@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Γλυφάδα
+                        </div>
+                        <div class="agent">
+                            Δημήτριος Ροκανάς
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Α. Λαζαράκη 43 και Γρ. Λαμπράκη 6, 16674,Γλυφάδα" target="_blank">
+                                Α. Λαζαράκη 43 και Γρ. Λαμπράκη 6, 16674
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-9551700 ">
+                                210-9551700
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-9551700
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn146@nnadvisors.gr ">
+                                nn146@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Καλλιθέα
+                        </div>
+                        <div class="agent">
+                            Οδυσσέας Κυπραίος
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Ελ. Βενιζέλου 327-331, 17674 ,Καλλιθέα" target="_blank">
+                                Λεωφ. Ελ. Βενιζέλου 327-331, 17674
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-9416270 ">
+                                210-9416270
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-9416270
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn113@nnadvisors.gr ">
+                                nn113@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Καλλιθέα
+                        </div>
+                        <div class="agent">
+                            Παντελής Πιτόγλου
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Συγγρού 196 και Χαροκόπου 2, 17671 ,Καλλιθέα" target="_blank">
+                                Λεωφ. Συγγρού 196 και Χαροκόπου 2, 17671
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel: 210-9466400 ">
+                                210-9466400
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-9466400
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn125@nnadvisors.gr ">
+                                nn125@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Καλλιθέα
+                        </div>
+                        <div class="agent">
+                            Νικόλαος Σταυράκης
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Συγγρού 196 και Χαροκόπου 2, 17671 ,Καλλιθέα" target="_blank">
+                                Λεωφ. Συγγρού 196 και Χαροκόπου 2, 17671
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-6255805 ">
+                                210-6255805
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-6255805
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn145@nnadvisors.gr">
+                                nn145@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Μαρούσι
+                        </div>
+                        <div class="agent">
+                            Κωνσταντίνος Μελισσουργός
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Κηφισίας 131, 15124,Μαρούσι" target="_blank">
+                                Λεωφ. Κηφισίας 131, 15124
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-4138888 ">
+                                210-4138888
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-4138888
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn151@nnadvisors.gr ">
+                                nn151@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Νέος Κόσμος
+                        </div>
+                        <div class="agent">
+                            Στέφανος Οθωναίος
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Καλλιρρόης 23, 11743, Νέος Κόσμος " target="_blank">
+                                Καλλιρρόης 23, 11743
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-9224490 ">
+                                210-9224490
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-9224490
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn133@nnadvisors.gr ">
+                                nn133@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Παλαιό Φάληρο
+                        </div>
+                        <div class="agent">
+                            Ιωάννης Κρεατσούλης
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Ποσειδώνος 73, 17562 , Παλαιό Φάληρο " target="_blank">
+                                Λεωφ. Ποσειδώνος 73, 17562
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel: 210-6234270 ">
+                                210-6234270
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-6234270
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn157@nnadvisors.gr ">
+                                nn157@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Χαλάνδρι
+                        </div>
+                        <div class="agent">
+                            Ευαγγελία Πιτόγλου
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Κηφισίας 296 και Ναυαρίνου, 15232 ,Χαλάνδρι" target="_blank">
+                                Λεωφ. Κηφισίας 296 και Ναυαρίνου, 15232
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-8115000 ">
+                                210-8115000
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-8115000
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn150@nnadvisors.gr ">
+                                nn150@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Χολαργός
+                        </div>
+                        <div class="agent">
+                            Φώτιος Βάσσης
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Μεσογείων 210 και Υμηττού 2, 15561 ,Χολαργός" target="_blank">
+                                Λεωφ. Μεσογείων 210 και Υμηττού 2, 15561
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel:210-4100113 ">
+                                210-4100113
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-4100113
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn159@nnadvisors.gr ">
+                                nn159@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 pb-3  text-center">
+                    <div class="office">
+                        <div class="location">
+                            Χολαργός
+                        </div>
+                        <div class="agent">
+                            Ελένη Μελαχροινού
+                        </div>
+                        <div class="address my-1">
+                            <a href="http://maps.google.com/maps?q=Λεωφ. Μεσογείων 210 και Υμηττού 2, 15561 ,Χολαργός" target="_blank">
+                                Λεωφ. Μεσογείων 210 και Υμηττού 2, 15561
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>T:</strong>
+                            <a href="tel: 210-6775008 ">
+                                210-6775008
+                            </a>
+                        </div>
+                        <div class="my-1">
+                            <strong>F:</strong>
+                            210-6775008
+                        </div>
+                        <div class="my-1">
+                            <a href="mailto:nn132@nnadvisors.gr ">
+                                nn132@nnadvisors.gr
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts about us section end -->
+
+
+<?php
+footer($str);
+?>
